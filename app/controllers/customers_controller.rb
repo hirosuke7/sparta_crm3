46bb1803415b678c565a1068b3bbe14a439c6c51
hirosuke7 <captif7_de_2_anjelot8@ezweb.ne.jp>
2018-03-18 11:50:04 +0900

    fix

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,52 @@
+class CustomersController < ApplicationController
+  before_action :set_customer, only: [ :show, :edit, :update, :destroy]
+  def index
+    @customers = Customer.page(params[:page])
+  end
+
+  def new
+    @customer = Customer.new
+  end
+
+  def create
+    @customer = Customer.new(customer_params)
+    if  @customer.save
+      redirect_to @customer
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if  @customer.update(customer_params)
+      redirect_to @customer
+    else
+      render :edit
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    @customer.destroy
+    redirect_to customers_path
+  end
+
+  def set_customer
+    @customer = Customer.find(params[:id])
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(
+      :family_name,
+      :given_name,
+      :email
+    )
+  end
+end
